7531c8fd535f59e8677dddc1debf138eb4c47e39
spozzz1233 <118517202+spozzz1233@users.noreply.github.com>
2024-04-24 21:11:32 +0300

    Update SDbContext.cs

--- Web/Data/SDbContext.cs
+++ Web/Data/SDbContext.cs
@@ -19,10 +19,7 @@ namespace Web.Data
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //Код выполняет конфигурацию модели данных для создания базы данных.
             base.OnModelCreating(modelBuilder);
-            //Метод ApplyConfiguration() применяет указанную конфигурацию для соответствующей сущности модели данных.
-            //Это позволяет настроить свойства каждой сущности, установить соответствующие ограничения, реализовать связи между сущностями и др.
             modelBuilder.ApplyConfiguration(new CustomerMap());
             modelBuilder.ApplyConfiguration(new EmployeeMap());
             modelBuilder.ApplyConfiguration(new OrderItemMap());
